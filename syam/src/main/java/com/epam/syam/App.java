9c6028a49e561b1cf00a83c3f4210996fec7ce5d
syam jason <syam jason@LAPTOP-2EESBBRI>
2020-02-10 21:43:10 +0530

    commit

---
+++ syam/src/main/java/com/epam/syam/App.java
@@ -0,0 +1,13 @@
+package com.epam.syam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
